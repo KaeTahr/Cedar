d69849e3f497591149da5714d9015caca6567a98
kaetahr <lostech@protonmail.com>
2025-08-12 13:27:55 +1000

    updated codegen mockup

--- code/Cedar/Main.hs
+++ code/Cedar/Main.hs
@@ -25,3 +25,29 @@ module Cedar.Main where
 
 -- handleReadError :: IOException -> IO String
 -- handleReadError _ = error "Error: Unable to read file."
+
+
+
+-- Test for CodeGen
+
+import System.IO
+import qualified Data.Char as Char
+import Cedar.CodeGen.EmitC
+
+student :: CBoundStruct
+student = CBoundStruct
+  { sName     = "Student"
+  , sSizeBits = 96  -- 12 bytes
+  , sFields   =
+      [ CField "name"   (CPrim Ptr)            (Range 0   64) (Just LE)
+      , CField "age"    (CPrim I8)             (Range 64   8) Nothing
+      , CField "maths"  (CPrim I8)             (Range 72   8) Nothing
+      , CField "physics"(CPrim I16)            (Range 80  16) (Just LE)
+      ]
+  }
+
+main :: IO ()
+main = do
+  writeFile "student_cedar.h" (emitHeader student)
+  writeFile "student_cedar.c" (emitImpl   student)
+  putStrLn "Wrote student_cedar.h / student_cedar.c"
